42fa86630b990318b194ae1ef8cc086202eccb9c
RdMaxes <yc.huang0522@gmail.com>
2014-06-29 11:18:02 +0800

    add "LCD" project

---
+++ Project/1-LCD/main.c
@@ -0,0 +1,23 @@
+#include <stm32f4xx.h>
+#include "led.h"
+
+static void delay(int32_t time)
+{
+	while(time--);
+}
+
+int main(void)
+{	
+	LED_Init();
+	while(1) 
+	{
+		LED_GREEN_ON();
+		LED_RED_ON();		
+		delay(800000);
+		LED_GREEN_OFF();
+		LED_RED_OFF();
+		delay(800000);
+	}
+
+	return 0;
+}
