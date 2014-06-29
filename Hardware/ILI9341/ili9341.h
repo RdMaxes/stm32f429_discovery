1407ba076759eb02e072f59dae025109ca3df323
RdMaxes <yc.huang0522@gmail.com>
2014-06-29 11:59:50 +0800

    define operation pins and used SPI for ILI9341 on stm32f429-DISCO

--- Hardware/ILI9341/ili9341.h
+++ Hardware/ILI9341/ili9341.h
@@ -1,5 +1,93 @@
+/*
+ *	ILI9341 library for LCD on STM32F429 Discovery, without LTDC hardware
+ * @Hardware Configuration:
+ * SPI Used: 	SPI5
+ * SPI MOSI:	PF9
+ * SPI SCK:	PF7	
+ * CS		PC2
+ * RESET	PD12
+ * D/C		PD13
+ */
 #ifndef ILI9341_H
 #define ILI9341_H
+
+//Chip Select(CS) pin
+#ifndef ILI9341_CS_PIN
+	#define ILI9341_CS_CLK			RCC_AHB1Periph_GPIOC
+	#define ILI9341_CS_PORT			GPIOC
+	#define ILI9341_CS_PIN			GPIO_Pin_2
+#endif
+//Data Command(D/C) pin
+#ifndef ILI9341_DC_PIN
+	#define ILI9341_DC_CLK			RCC_AHB1Periph_GPIOD
+	#define ILI9341_DC_PORT			GPIOD
+	#define ILI9341_DC_PIN			GPIO_Pin_13
+#endif
+//Reset pin (RST) pin
+#ifndef ILI9341_RST_PIN
+	#define ILI9341_RST_CLK			RCC_AHB1Periph_GPIOD
+	#define ILI9341_RST_PORT		GPIOD
+	#define ILI9341_RST_PIN			GPIO_Pin_12
+#endif
+
+#define ILI9341_RST_SET				GPIO_SetBits(ILI9341_RST_PORT, ILI9341_RST_PIN)
+#define ILI9341_RST_RESET			GPIO_ResetBits(ILI9341_RST_PORT, ILI9341_RST_PIN)
+#define ILI9341_CS_SET				GPIO_SetBits(ILI9341_CS_PORT, ILI9341_CS_PIN)
+#define ILI9341_CS_RESET			GPIO_ResetBits(ILI9341_CS_PORT, ILI9341_CS_PIN)
+#define ILI9341_DC_SET				GPIO_SetBits(ILI9341_DC_PORT, ILI9341_DC_PIN)
+#define ILI9341_DC_RESET			GPIO_ResetBits(ILI9341_DC_PORT, ILI9341_DC_PIN)
+
+//LCD settings
+#define ILI9341_WIDTH 				240
+#define ILI9341_HEIGHT				320
+#define ILI9341_PIXEL				76800
+
+//Colors
+#define ILI9341_COLOR_WHITE			0xFFFF
+#define ILI9341_COLOR_BLACK			0x0000
+#define ILI9341_COLOR_RED			0xF800
+#define ILI9341_COLOR_GREEN			0x07E0
+#define ILI9341_COLOR_GREEN2			0xB723
+#define ILI9341_COLOR_BLUE			0x001F
+#define ILI9341_COLOR_BLUE2			0x051D
+#define ILI9341_COLOR_YELLOW			0xFFE0
+#define ILI9341_COLOR_ORANGE			0xFBE4
+#define ILI9341_COLOR_CYAN			0x07FF
+#define ILI9341_COLOR_MAGENTA			0xA254
+#define ILI9341_COLOR_GRAY			0x7BEF 
+#define ILI9341_COLOR_BROWN			0xBBCA
+
+//Commands
+#define ILI9341_RESET				0x01
+#define ILI9341_SLEEP_OUT			0x11
+#define ILI9341_GAMMA				0x26
+#define ILI9341_DISPLAY_OFF			0x28
+#define ILI9341_DISPLAY_ON			0x29
+#define ILI9341_COLUMN_ADDR			0x2A
+#define ILI9341_PAGE_ADDR			0x2B
+#define ILI9341_GRAM				0x2C
+#define ILI9341_MAC				0x36
+#define ILI9341_PIXEL_FORMAT			0x3A
+#define ILI9341_WDB				0x51
+#define ILI9341_WCD				0x53
+#define ILI9341_RGB_INTERFACE			0xB0
+#define ILI9341_FRC				0xB1
+#define ILI9341_BPC				0xB5
+#define ILI9341_DFC				0xB6
+#define ILI9341_POWER1				0xC0
+#define ILI9341_POWER2				0xC1
+#define ILI9341_VCOM1				0xC5
+#define ILI9341_VCOM2				0xC7
+#define ILI9341_POWERA				0xCB
+#define ILI9341_POWERB				0xCF
+#define ILI9341_PGAMMA				0xE0
+#define ILI9341_NGAMMA				0xE1
+#define ILI9341_DTCA				0xE8
+#define ILI9341_DTCB				0xEA
+#define ILI9341_POWER_SEQ			0xED
+#define ILI9341_3GAMMA_EN			0xF2
+#define ILI9341_INTERFACE			0xF6
+#define ILI9341_PRC				0xF7
 
 //function declaration
 
