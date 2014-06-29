3a3e0ada903bfa3238ef520e9371e45415b3b941
RdMaxes <yc.huang0522@gmail.com>
2014-06-29 14:59:26 +0800

    crear "2-USART2" project

---
+++ Project/2-USART2/main.c
@@ -0,0 +1,30 @@
+#include <stm32f4xx.h>
+#include "led.h"
+#include "myfont.h"
+#include "ili9341.h"
+
+
+static void delay(int32_t time)
+{
+	while(time--);
+}
+
+int main(void)
+{	
+	LED_Init();
+	ILI9341_Init();
+
+	ILI9341_Fill(ILI9341_COLOR_BLUE);
+	ILI9341_Puts(0,0,"LCD string test",&MY_Font_11x18,ILI9341_COLOR_WHITE,ILI9341_COLOR_BLUE);
+	while(1) 
+	{	
+		LED_GREEN_ON();
+		LED_RED_OFF();		
+		delay(800000);
+		LED_GREEN_OFF();
+		LED_RED_ON();
+		delay(800000);
+	}
+
+	return 0;
+}
---
+++ Project/2-USART2/st_util_init.gdb
@@ -0,0 +1,4 @@
+file firmware.elf
+target remote :4242
+monitor reset halt
+c
