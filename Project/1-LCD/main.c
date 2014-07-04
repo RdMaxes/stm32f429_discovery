424f28eaecb9d6c20b513d4ceb398941d46390ec
RdMaxes <yc.huang0522@gmail.com>
2014-07-04 15:26:09 +0800

    add myfont.h and myprintf.h

--- Project/1-LCD/main.c
+++ Project/1-LCD/main.c
@@ -1,8 +1,9 @@
 #include <stm32f4xx.h>
-#include "string.h"
 #include "led.h"
 #include "myfont.h"
+#include "myprintf.h"
 #include "ili9341.h"
+#include "usart1.h"
 
 
 static void delay(int32_t time)
@@ -11,15 +12,13 @@ static void delay(int32_t time)
 }
 
 int main(void)
-{	
-	char str1[] = "hello, world!";
-	char str2[] = "This is string test";	
 	LED_Init();
 	ILI9341_Init();
-//	ILI9341_Puts(0,0,"LCD string test",&MY_Font_11x18,ILI9341_COLOR_WHITE,ILI9341_COLOR_BLUE);
-	strcpy(str1,str2);
+	Usart1_Init(9600);
+	Myprintf_Init(0x00,myputc);
+
 	while(1) 
-	{	
+	{		
 		LED_GREEN_ON();
 		LED_RED_OFF();		
 		delay(800000);
